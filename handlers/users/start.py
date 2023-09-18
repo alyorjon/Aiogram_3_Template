e6972bf2549a7804e84c77636d8a322041f11671
alyorjon <alyorjon>
2023-09-18 09:16:12 +0500

    new

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -13,19 +13,4 @@ async def start_handler(message: types.Message):
 @dp.message()
 async def start_handler(message: types.Message):
     text = detect_alphabet(message.text)
-    if text == "Lotin va Krill":
-        lotinkrill = translate_text('cyrtolat', message.text)
-        await message.answer(lotinkrill['result'])
-        print(lotinkrill)
-        await asyncio.sleep(0.001)
-        lotinkrill = translate_text('lattocyr', message.text)
-        await message.answer(lotinkrill['result'])
-        print(lotinkrill)
-    elif text == "Lotin":
-        lotinkrill = translate_text('lattocyr', message.text)
-        await message.answer(lotinkrill['result'])
-    elif text == "Krill":
-        lotinkrill = translate_text('cyrtolat', message.text)
-        await message.answer(lotinkrill['result'])
-    else:
-        await message.answer(message.text)
+    pass
